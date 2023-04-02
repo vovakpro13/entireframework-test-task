2a26218d6ce5bfeb80b41fbdeb6b78396d4f71f0
VolodymyrMelnykInoxoft <volodymyr.melnyk@inoxoft.com>
2023-04-02 19:19:28 +0300

    fixed config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   return {
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        external: ["styled-components"],
+      },
+    },
   };
 });
