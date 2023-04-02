b447fb3ae5e8dd2fdcceb56a6c4cbf7a414b422b
VolodymyrMelnykInoxoft <volodymyr.melnyk@inoxoft.com>
2023-04-02 20:08:21 +0300

    fixed issue with vite + styled-components

--- src/components/ViewWrapper.ts
+++ src/components/ViewWrapper.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 const ViewWrapper = styled("div")`
   width: 100vw;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import babelMacros from "vite-plugin-babel-macros";
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
-    build: {
-      rollupOptions: {
-        external: ["styled-components"],
-      },
-    },
+    plugins: [react(), babelMacros()],
   };
 });
